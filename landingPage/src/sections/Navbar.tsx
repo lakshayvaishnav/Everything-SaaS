e69e8120ea5486c2f95d09dbcd7b1cf1385f41a0
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2024-11-20 14:41:35 +0530

    ✅ navbar for 3 sizes

--- landingPage/src/sections/Navbar.tsx
+++ landingPage/src/sections/Navbar.tsx
@@ -12,13 +12,26 @@ const navLinks = [
 export default function Navbar() {
     return (
         <section className="py-4 ">
-            <div className="container">
+            <div className="container max-w-5xl">
                 <div
-                    className="grid grid-cols-2 border border-white/15 rounded-full px-4 md:pr-2  p-2 items-center
+                    className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full px-4 md:pr-2  p-2 items-center
                 "
                 >
                     <div>
-                        <Image src={Logoimage} alt="layers logo" className="h-9 w-auto md:h-auto " />
+                        <Image
+                            src={Logoimage}
+                            alt="layers logo"
+                            className="h-9 w-auto md:h-auto "
+                        />
+                    </div>
+                    <div className="lg:flex items-center justify-center hidden ">
+                        <nav className="flex gap-6 font-medium ">
+                            {navLinks.map((link) => (
+                                <a href={link.href} key={link.label}>
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
                     </div>
                     <div className="flex justify-end gap-4">
                         <svg
@@ -37,8 +50,8 @@ export default function Navbar() {
                             <line x1="3" y1="6" x2="21" y2="6"></line>
                             <line x1="3" y1="18" x2="21" y2="18"></line>
                         </svg>
-                        <Button variant="secondary">Log In</Button>
-                        <Button variant="primary">Sign Up</Button>
+                        <Button variant="secondary" className="hidden md:inline-flex items-center">Log In</Button>
+                        <Button variant="primary" className="hidden md:inline-block">Sign Up</Button>
                     </div>
                 </div>
             </div>
